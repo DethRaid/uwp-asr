0daa54660cd853ee458dac12e506c66c8c9f4a7c
David Dubois <dd4942@rit.edu>
2017-03-16 22:20:36 -0400

    Think I finally got everything set up

---
+++ fastText/Class1.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+namespace FastText
+{
+    public ref class Class1 sealed
+    {
+    public:
+        Class1();
+    };
+}
--- fastText/pch.h
+++ fastText/pch.h
@@ -1,10 +1,4 @@
 ﻿#pragma once
 
-#include "targetver.h"
-
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-
-#define NOMINMAX
-#include <windows.h>
+#include <collection.h>
+#include <ppltasks.h>
